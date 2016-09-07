7cca1402f9ae46c0a32374ad7953d04e2013c596
Brett Macy <bam03584@aol.com>
2016-09-07 08:00:57 -0400

    added car animation

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -4,13 +4,15 @@ class Animation extends Component {
 
   render () {
     return <div className='animation-container'>
-    <h1>Follow The Bouncing Sassy Ball!</h1>
+      <h1>Follow The Bouncing Sassy Ball!</h1>
       <div id='ballWrapper'>
-        <div id='ball' style={{'position': 'absolute'}} />
-        <div id='ballShadow' />
+        <div id='ball' style={{'position': 'absolute'}}></div>
+        <div id='ballShadow'></div>
+      </div>
+      <div className='car-container'>
+        <div className='car'></div>
       </div>
     </div>
   }
 }
-
 export default Animation
